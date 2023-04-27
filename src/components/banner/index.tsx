890e73dfecb298f0aea0371c5398acb5dac01a5c
httpE2Barao <elias.barao@hotmail.com>
2023-04-27 18:37:55 -0300

    iniciando projeto CodeChella2023

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,14 @@
+import './banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <aside>
+                <h1>Boas-vindas ao</h1>
+                <h1>ao CodeChella2023!</h1>
+            </aside>
+            <img src='./imagens/Tema Verão/Desktop/Imagens/1 - Banner -Homepage.png'/>
+        </div>
+    )
+}
+export default Banner
---
+++ src/components/header/index.tsx
@@ -0,0 +1,16 @@
+import './header.css'
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <img src='./imagens/Logo-branco.png'/>
+            <ul>
+                <li>A experiência</li>
+                <li>Mapa de Setores</li>
+                <li>Informações</li>
+                <li>Ingresso</li>
+            </ul>
+        </header>
+    )
+}
+export default Header
