14d7c04e02c0d1409977e9626490f5e135df7b32
httpE2Barao <elias.barao@hotmail.com>
2023-06-13 19:17:55 -0300

    Update Experiencia.tsx

--- src/pages/Experiencia.tsx
+++ src/pages/Experiencia.tsx
@@ -19,12 +19,12 @@ const Experiencia = () => {
         text="Nosso evento tenta abraçar um público mais amplo em todos os sentidos, de todas as idades, corpos, gostos e pensamentos!  Isso está presente no  espaço físico, na sinalização, no treinamento da equipe de apoio, na comunicação em libras, braile, visual e tátil. Além disso, dispomos de ingressos gratuitos para pessoas com deficiência e acompanhantes, pessoas idosas e crianças!"
 
         titulo1="Sustentabilidade"
-        src1="./imagens/pngs/2 - A experiencia2.png"
+        src1="./imagens/pngs/2 - A Experiencia2.png"
         alt1="Foto de uma sacola com itens para reciclagem"
         text1="Queremos estar aqui celebrando daqui a 50, 100 anos! Por isso, levamos o futuro do nosso planeta a sério. Nosso festival é carbono zero, com utilização de copos retornáveis, coleta e destinação adequada de resíduos, e transporte coletivo."
 
         titulo2="Atrações"
-        src2="./imagens/pngs/2 - A experiencia3.png"
+        src2="./imagens/pngs/2 - A Experiencia3.png"
         alt2="Foto de uma roda gigante"
         text2="Intervenções artísticas e culturais para todos os gostos! Durante os intervalos dos shows, nossa experiência contará com recreação para crianças, oficina de cerâmica, origami, circo e aquarela! Além disso, nosso espaço contra com 3 praças de alimentação, roda gigante e outros brinquedos!"
       />
