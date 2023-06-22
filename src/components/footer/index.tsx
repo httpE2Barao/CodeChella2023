585e6c4303c088400e9d613d4e1e3136bbd66b90
httpE2Barao <elias.barao@hotmail.com>
2023-06-22 16:18:52 -0300

    ajustando CSS

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,17 +6,17 @@ const Footer = () => {
         <div className='footer'>
             <div className='logo_container'>
                 <img className='logo' src="./imagens/Logos/Logo cinza.svg" alt="Logo CodeChella" />
+                <p>Desenvolvido por <a target='blank' href="https://github.com/httpE2Barao">
+                    Elias Barão.</a>
+                </p>
+            </div>
+            <div className='aside'>
                 <div>
                     <p>Acesse as nossas contas:</p>
                     <img src="./imagens/Ícones/svgs/Ícone whatsapp.svg" alt="Ícone Whatsapp" />
                     <img src="./imagens/Ícones/svgs/Ícone Twitch.svg" alt="Ícone Twitch" />
                     <img src="./imagens/Ícones/svgs/Ícone instagram.svg" alt="Ícone Instagram" />
                 </div>
-            </div>
-            <div className='aside'>
-                <p>Desenvolvido por <a target='blank' href="https://github.com/httpE2Barao">
-                    Elias Barão.</a>
-                </p>
             </div>
         </div>
     )
